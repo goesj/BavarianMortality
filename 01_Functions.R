1b86fafddc4360bed55934b935d4c50b071b4619
’goesj’ <goesjulius@gmail.com>
2022-08-19 10:29:50 +0200

    Added Empirical CDF

--- 01_Functions.R
+++ 01_Functions.R
@@ -230,6 +230,16 @@ CRPSEmp <- function(ObservedCount, FCMat, Exposure){
   return(DSSEmp)
 }
 
+#Empirical CDF
+EmpCDFFun <- function(ObservedCount, FCMat, Exposure){
+  N <- length(ObservedCount)
+  ECDF <- numeric(N)
+  for (i in 1:N) {
+    ECDF[i] <- mean(ppois(ObservedCount[i],exp(FCMat[,i])*Exposure[i]))
+  }
+  return(ECDF)
+}
+
 #Pit Histogram##
 ## Taken from Riebler INLA Group
 # https://groups.google.com/g/r-inla-discussion-group/c/eSwZJ5Iegcc
