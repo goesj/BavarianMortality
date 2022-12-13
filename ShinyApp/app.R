d7c90ebb2a671825e6a99aa95aa500740618fbe5
’goesj’ <goesjulius@gmail.com>
2022-12-13 13:49:59 +0100

    Fix Shiny app

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -1,4 +1,4 @@
-library(shiny); library(RColorBrewer); library(cowplot); library(ggridges); 
+library(shiny); library(RColorBrewer); library(cowplot); library(ggridges);
 library(ggpubr); library(ggplot2); library(tidyverse); library(ggdist)
 
 ## First load the data and functions needed
@@ -7,7 +7,7 @@ source("../01_Functions.R")
 load(file = "../Data/TotalData.RData") #load Data
 load(file = "../Data/LifeExpFrameStacking.RData") #Load Results
 
-RegionList <- TotalData$RegionName %>% unique() %>% as.list()
+RegionList <- TotalData$RegionNumber %>% unique() %>% as.list()
 
 NamesEnglish <- gsub("(Lkr)", "Region", unique(TotalData$RegionName)) #Change Lkr to Region
 NamesEnglish <- gsub("(Krfr.St)", "City", NamesEnglish) #Change Krfr. St to City
@@ -36,7 +36,7 @@ ui <- fluidPage(
                        step = 1, sep=""))
     ),
   fluidRow(
-    column(8, offset = 3, 
+    column(8, offset = 3,
            h4("Life Expecancy at Birth", align="left"),
            tableOutput("LifeTable"))
   ),
@@ -47,7 +47,6 @@ ui <- fluidPage(
 # Define server logic required to draw plots
 server <- function(input, output) {
   # 1. Output Map Plot
-  
   output$RegionPlot <- renderPlot({ 
     Bayern <- Bayern %>% 
       mutate("LifeExpStack_F" = LifeExpFrameFemaleStacking %>% 
@@ -60,9 +59,9 @@ server <- function(input, output) {
              "Rank_M" = rank(-LifeExpStack_F, ties.method = "random"),
              "SelectedRegion"=ifelse(RS == input$Region,"Yes","No"),
              "alphaRegion" = ifelse(RS==input$Region, 1,0.2))
-      
+    
     #Plot for Females
-      g1F <- ggplot(data = Bayern) +
+    g1F <- ggplot(data = Bayern) +
       geom_sf(aes(fill = LifeExpStack_F, color = SelectedRegion,alpha=alphaRegion), size = 0) +
       scale_color_manual(values = c("No"=NA,"Yes"="red"), guide="none")+
       scale_alpha_continuous(range=c(0.2,1), guide="none")+
@@ -72,23 +71,23 @@ server <- function(input, output) {
       theme_void() + 
       theme(plot.title = element_text(size = 15, face = "bold"),
             legend.position = "bottom") 
-      
-      #Plot for Males
-      g1M <- ggplot(data = Bayern) +
-        geom_sf(aes(fill = LifeExpStack_M, color = SelectedRegion,alpha=alphaRegion), size = 0) +
-        scale_color_manual(values = c("No"=NA,"Yes"="red"), guide="none")+
-        scale_alpha_continuous(range=c(0.2,1), guide="none")+
-        scale_fill_gradientn(name="LifeExpectany",colors=Mypal, 
-                             labels = function(x) {round(x,0)})+
-        ggtitle("Male")+
-        theme_void() + 
-        theme(plot.title = element_text(size = 15, face = "bold"),
-              legend.position = "bottom") 
-      
-      #adding both
-      plot_grid(g1F, g1M)
-      
-      
+    
+    #Plot for Males
+    g1M <- ggplot(data = Bayern) +
+      geom_sf(aes(fill = LifeExpStack_M, color = SelectedRegion,alpha=alphaRegion), size = 0) +
+      scale_color_manual(values = c("No"=NA,"Yes"="red"), guide="none")+
+      scale_alpha_continuous(range=c(0.2,1), guide="none")+
+      scale_fill_gradientn(name="LifeExpectany",colors=Mypal, 
+                           labels = function(x) {round(x,0)})+
+      ggtitle("Male")+
+      theme_void() + 
+      theme(plot.title = element_text(size = 15, face = "bold"),
+            legend.position = "bottom") 
+    
+    #adding both
+    plot_grid(g1F, g1M)
+    
+    
   })
   
   #2. Table Output
@@ -99,14 +98,16 @@ server <- function(input, output) {
         select(1:5) %>% #only first 5 columns
         pivot_wider(id_cols = 1:2, names_from = 5, values_from = 3:4) %>% #pivot wide
         select(c(1:4,6,5)) %>% #reorder
-        mutate("Width 80"=PiUp_0.8-PiLo_0.8), 
+        mutate("Width 50%"=PiUp_0.5-PiLo_0.5,
+               "Width 80%"=PiUp_0.8-PiLo_0.8), 
       #Male
       LifeExpFrameMaleStacking %>% filter(RegNumber== input$Region & Year == input$Year) %>%
         slice(1:2) %>% #only first two rows
         select(1:5) %>% #only first 5 columns
         pivot_wider(id_cols = 1:2, names_from = 5, values_from = 3:4) %>% #pivot wide
         select(c(1:4,6,5)) %>% #reorder
-        mutate("Width 80"=PiUp_0.8-PiLo_0.8)
+        mutate("Width 50%"=PiUp_0.5-PiLo_0.5,
+               "Width 80%"=PiUp_0.8-PiLo_0.8)
     ) %>% mutate("Sex"=c("Female","Male"),.before=1) %>%
       rename(., "10%" = PiLo_0.8,
              "25%"=PiLo_0.5,
@@ -114,11 +115,12 @@ server <- function(input, output) {
              "90%" = PiUp_0.8) %>% 
       mutate_if(is.numeric, ~round(., 2)) # round numeric columns (later transformed into character)
     
-    as_tibble(cbind(names(Table), t(Table))) %>% #transpose
-      rename(.," " = V1,                     #rename columns
-             "Female"=V2,
-             "Male" =V3) %>%
-      slice(-1)
+    #Transpose Table and save as tible
+    # as_tibble(cbind(names(Table), t(Table))) %>% #transpose
+    #   rename(.," " = V1,                     #rename columns
+    #         "Female"=V2,
+    #          "Male" =V3) %>%
+    #   slice(-1)
   })
   
   #3. Output Line Plot of Life Expectancy
@@ -152,7 +154,7 @@ server <- function(input, output) {
                       col = "#011f4b",alpha=0.5,size=0.8,group=1)+
       scale_fill_manual(values = c("0.8w"="#e5cce5","0.5w"="#bf7fbf",
                                    "0.8m"="#d1e1ec", "0.5m"="#b3cde0"))+ #values of ribbon (second value is 50%PI)
-      
+      geom_vline(xintercept = as.character(input$Year), lty = 4)+
       theme_bw()+
       ylab("Life Expectancy")+xlab("Year")+
       scale_x_discrete(breaks=c(2001,2010,2020,2030))+
